a14726cb7aa36b1dca185e409e728da05334577e
mildchae <mildchaee@gmail.com>
2020-06-22 23:14:40 +0900

    flat list

--- App.js
+++ App.js
@@ -1,28 +1,103 @@
 import React from 'react';
-import { Text, View, ScrollView, Image, TextInput } from 'react-native';
+import {
+  SafeAreaView,
+  TouchableOpacity,
+  FlatList,
+  StyleSheet,
+  Text,
+} from 'react-native';
 import Constants from 'expo-constants';
 
-function HelloWorldApp() {
+
+const handleRefresh = () => {
+  console.log("리프레시");
+  this.setState({
+    refreshing: true,
+    isLoad: false,
+    data : [],
+    page: 1,
+  }, () => {this.getUsedItemList()});
+}
+
+
+const DATA = [
+  {
+    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+    title: 'First Item',
+  },
+  {
+    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+    title: 'Second Item',
+  },
+  {
+    id: '58694a0f-3da1-471f-bd96-145571e29d72',
+    title: 'Third Item',
+  },
+];
+
+function Item({ id, title, selected, onSelect }) {
   return (
-    <View  style={{flex:1, justifyContent:"flex-end", alignItems:"center", marginTop: Constants.statusBarHeight}}>
-      <ScrollView style={{flexGrow: 1}}>
-        <Text style={{}}>hello world!</Text>
-        <View>
-          <Text>More hello!</Text>
-          <Image 
-          source = {{ uri: "https://reactnative.dev/docs/assets/p_cat2.png"}}
-          style= {{width:200, height:200}} />
-        </View>
-        <TextInput 
-          style={{
-            height: 40,
-            borderColor: 'gray',
-            borderWidth: 1
-          }}
-          defaultValue = "input somthing"
-        />
-      </ScrollView>
-    </View>
+    <TouchableOpacity
+      onPress={() => onSelect(id)}
+      style={[
+        styles.item,
+        { backgroundColor: selected ? '#6e3b6e' : '#f9c2ff' },
+      ]}
+    >
+      <Text style={styles.title}>{title}</Text>
+    </TouchableOpacity>
+  );
+}
+
+
+function App() {
+  const [selected, setSelected] = React.useState(new Map());
+
+  const onSelect = React.useCallback(
+    id => {
+      const newSelected = new Map(selected);
+      newSelected.set(id, !selected.get(id));
+
+      setSelected(newSelected);
+    },
+    [selected],
+  );
+
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={DATA}
+        renderItem={({ item }) => (
+          <Item
+            id={item.id}
+            title={item.title}
+            selected={!!selected.get(item.id)}
+            onSelect={onSelect}
+          />
+        )}
+        keyExtractor={item => item.id}
+        extraData={selected}
+      />
+    </SafeAreaView>
   )
 }
-export default HelloWorldApp;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: Constants.statusBarHeight,
+  },
+  item: {
+    backgroundColor: '#f9c2ff',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  title: {
+    fontSize: 32,
+  },
+});
+
+
+export default App;
